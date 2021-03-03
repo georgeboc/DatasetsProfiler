ca23d49e3005f3ffead666006f0236ab022811e8
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-03-03 15:25:07 +0000

    Enhances MOOC log parser strategy

--- datasets_evaluation/src/parsers/mooc_log_parser_strategy.py
+++ datasets_evaluation/src/parsers/mooc_log_parser_strategy.py
@@ -12,11 +12,13 @@ class MoocLogParserStrategy(ParserStrategy):
 
     def parse(self, row):
         row_string = row[0]
-        enroll_id_s, username_s, course_id_s, session_id_s, action_s, object_s, time_s, truth_s = \
-            self._parser_commons.nullify_missing_fields(row_string.split(','))
+        parsed_parameters = [None, ]*9
+        for parameter_index, parameter in enumerate(self._parser_commons.nullify_missing_fields(row_string.split(','))):
+            parsed_parameters[parameter_index] = parameter
+        enroll_id_s, username_s, course_id_s, session_id_s, action_s, object_s, _, date_time_s, truth_s = parsed_parameters
         enroll_id = int(enroll_id_s) if enroll_id_s else None
         username = int(username_s) if username_s else None
-        date_time = datetime.strptime(time_s, '%Y-%m-%dT%H:%M:%S') if time_s else None
+        date_time = datetime.strptime(date_time_s, '%Y-%m-%dT%H:%M:%S') if date_time_s else None
         truth = int(truth_s) if truth_s else None
         return enroll_id, username, course_id_s, session_id_s, action_s, object_s, date_time, truth
 
