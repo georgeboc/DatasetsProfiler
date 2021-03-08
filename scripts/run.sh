bbce61884e52566c45cacc509760a260f45b46f9
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-03-08 02:51:22 +0000

    Deletes scripts

--- scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#python ../dist/DatasetsEvaluation-1.0-py3.9.egg
-
-#root@6a8abdd30fc9:/files/datasets_evaluation/src# cat ../parameters_lists/pipeline_jobs_automated_error_windows.inp | python __main__.py
-
-cat ../input/pipeline_jobs_automated_sent_windows.inp | python __main__.py
