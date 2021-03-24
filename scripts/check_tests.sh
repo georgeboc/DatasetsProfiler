71765a353e5b644677b6c5d0219d5f3224727dfc
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-03-24 05:32:26 +0000

    Adds cleanup and running of tests to check_tests script

--- scripts/check_tests.sh
+++ scripts/check_tests.sh
@@ -7,9 +7,14 @@ export RED='\033[0;31m'
 export GREEN='\033[92m'
 export END='\033[0m'
 
+rm -r ../output/samples_test/*
+
+./run.sh ../parameters/parameters_integration_test
+
 if [ "$(diff -bur $CORRECT $TEST | grep -v "Only in" -c)" -eq 0 ];
 then
   printf "${GREEN}All tests passed successfully! ${END}\n"
 else
+  diff -bur $CORRECT $TEST | grep -v "Only in" | less
   printf "${RED}Error passing the tests ${END}\n"
 fi;
