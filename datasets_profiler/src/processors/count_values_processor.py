b7f24c3fa7b48b53bda617293209ca74e4624f39
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-04-13 18:57:18 +0200

    Adds string casting and sorting in this order

--- datasets_profiler/src/processors/count_values_processor.py
+++ datasets_profiler/src/processors/count_values_processor.py
@@ -1,4 +1,5 @@
-from pyspark.sql.functions import lit, sum
+from pyspark.sql.functions import lit, sum, col, when, unix_timestamp
+from pyspark.sql.types import StringType
 
 from datasets_profiler.src.instrumentation.call_tracker import instrument_call
 from datasets_profiler.src.results.count_values_results import CountValuesResults
@@ -8,6 +9,8 @@ class CountValuesProcessor:
     VALUE = "Value"
     COUNT = "Count"
     FREQUENCY = "Frequency"
+    TIMESTAMP = "timestamp"
+    LONG = "long"
 
     def __init__(self, column_statistics_calculator, spark_configuration, call_tracker):
         self._column_statistics_calculator = column_statistics_calculator
@@ -24,6 +27,8 @@ class CountValuesProcessor:
             .cache()
         rows_count = values_frequencies_data_frame_cached.count()
         is_unique = rows_count == column_data_frame_cached.count()
-        return CountValuesResults(data_frame=values_frequencies_data_frame_cached.orderBy(self.VALUE),
+        string_casted_data_frame = values_frequencies_data_frame_cached.withColumn(self.VALUE, col(self.VALUE).cast(StringType()))
+        sorted_data_frame = string_casted_data_frame.orderBy(self.VALUE)
+        return CountValuesResults(data_frame=sorted_data_frame,
                                   rows_count=rows_count,
                                   is_unique=is_unique)
