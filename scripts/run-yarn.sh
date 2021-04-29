307a11b3b75b185c4917b29b979bfeed3d92774e
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-04-30 00:13:11 +0200

    Changes from deployment to deploy-mode in run-yarn script

--- scripts/run-yarn.sh
+++ scripts/run-yarn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip3 install --force-reinstall DatasetsProfiler-1.*-py3-none-any.whl
-spark-submit --master yarn --deployment cluster --packages org.apache.spark:spark-avro_2.12:3.0.1 \
-  --py-files DatasetsProfiler-1.*-py3-none-any.whl /usr/local/lib/python3.8/dist-packages/datasets_profiler/__main__.py "$@"
+pip3 install --force-reinstall ../dist/DatasetsProfiler-1.*-py3-none-any.whl
+spark-submit --master yarn --deploy-mode cluster --packages org.apache.spark:spark-avro_2.12:3.0.1 \
+  --py-files ../dist/DatasetsProfiler-1.*-py3-none-any.whl /usr/local/lib/python3.8/dist-packages/datasets_profiler/__main__.py "$@"
