68f8b849379d301129f9f3366325613b1ca4b38e
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-04-30 00:09:15 +0200

    Specifies python version 3 in scripts

--- scripts/build-wheel.sh
+++ scripts/build-wheel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ..
-python setup.py bdist_wheel
+python3 setup.py bdist_wheel
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip install --force-reinstall DatasetsProfiler-1.*-py3-none-any.whl
+pip3 install --force-reinstall DatasetsProfiler-1.*-py3-none-any.whl
 spark-submit --packages org.apache.spark:spark-avro_2.12:3.0.1 \
   --py-files DatasetsProfiler-1.*-py3-none-any.whl /usr/local/lib/python3.8/dist-packages/datasets_profiler/__main__.py "$@"
