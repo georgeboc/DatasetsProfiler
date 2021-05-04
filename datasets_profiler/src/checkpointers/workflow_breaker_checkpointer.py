64a724bc15476bc43787d05ac1a8293e978edc84
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-05-05 01:41:38 +0200

    Adds two different checkpoint paths for the local and cluster executions

--- datasets_profiler/src/checkpointers/workflow_breaker_checkpointer.py
+++ datasets_profiler/src/checkpointers/workflow_breaker_checkpointer.py
@@ -7,13 +7,15 @@ LOG = getLogger(__name__)
 
 
 class WorkflowBreakerCheckpointer:
-    CHECKPOINTS_PATH = "../temporal/"
+    LOCAL_CHECKPOINTS_PATH = "../temporal/"
+    CLUSTER_CHECKPOINTS_PATH = "/user/bochileanu/temporal/"
     CHECKPOINT_PREFIX = "checkpoint"
 
-    def __init__(self, dataframe_serializer_deserializer, filesystem, call_tracker):
+    def __init__(self, dataframe_serializer_deserializer, filesystem, call_tracker, local_execution_checker):
         self._dataframe_serializer_deserializer = dataframe_serializer_deserializer
         self._filesystem = filesystem
         self._call_tracker = call_tracker
+        self._local_execution_checker = local_execution_checker
 
     @instrument_call
     def checkpoint(self, data_frame):
@@ -24,7 +26,12 @@ class WorkflowBreakerCheckpointer:
         return self._dataframe_serializer_deserializer.deserialize(checkpoint_name)
 
     def clean_all_checkpoints(self):
-        self._filesystem.remove_recursively(self.CHECKPOINTS_PATH)
+        self._filesystem.remove_recursively(self._get_checkpoints_path())
 
     def _generate_filename(self, data_frame):
-        return f"{self.CHECKPOINTS_PATH}/{self.CHECKPOINT_PREFIX}_{hash(data_frame)}_{datetime.now()}"
+        return f"{self._get_checkpoints_path()}/{self.CHECKPOINT_PREFIX}_{hash(data_frame)}_{datetime.now()}"
+
+    def _get_checkpoints_path(self):
+        if self._local_execution_checker.is_local_execution():
+            return self.LOCAL_CHECKPOINTS_PATH
+        return self.CLUSTER_CHECKPOINTS_PATH
