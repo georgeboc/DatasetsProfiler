6128336e389e01c8d1b223fe4d496bf99224e102
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-05-05 20:26:04 +0200

    Solves problem with conf in Spark

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -6,13 +6,13 @@ export DATASETS_PROFILER_WHEEL_PATH="dist/DatasetsProfiler-1.*-py3-none-any.whl"
 pip3 install --force-reinstall $DATASETS_PROFILER_WHEEL_PATH
 spark-submit \
 --master local[*] \
---conf spark.driver.memory 2526 \
---conf spark.yarn.driver.memoryOverhead 512 \
---conf spark.executor.memory 2526 \
---conf spark.yarn.executor.memoryOverhead 512 \
---conf spark.driver.cores 1 \
---conf spark.executor.cores 1 \
---conf spark.executor.instances 23 \
+--conf spark.driver.memory=2526 \
+--conf spark.yarn.driver.memoryOverhead=512 \
+--conf spark.executor.memory=2526 \
+--conf spark.yarn.executor.memoryOverhead=512 \
+--conf spark.driver.cores=1 \
+--conf spark.executor.cores=1 \
+--conf spark.executor.instances=23 \
 --packages org.apache.spark:spark-avro_2.12:3.0.1 \
 --py-files $DATASETS_PROFILER_WHEEL_PATH \
 __main__.py --parameters-path "$@" --local
