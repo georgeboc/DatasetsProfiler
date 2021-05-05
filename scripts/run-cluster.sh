48b4ddce761ec937bb8bf58318cc4acbb5e58602
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-05-05 02:38:32 +0200

    Makes driver memory be 24 GiB

--- scripts/run-cluster.sh
+++ scripts/run-cluster.sh
@@ -4,5 +4,5 @@
 export DATASETS_PROFILER_WHEEL_PATH="dist/DatasetsProfiler-1.*-py3-none-any.whl"
 
 pip3 install --force-reinstall $DATASETS_PROFILER_WHEEL_PATH
-spark-submit --master yarn --deploy-mode cluster --packages org.apache.spark:spark-avro_2.12:3.0.1 \
+spark-submit --master yarn --deploy-mode cluster --driver-memory 24g --packages org.apache.spark:spark-avro_2.12:3.0.1 \
   --py-files $DATASETS_PROFILER_WHEEL_PATH __main__.py --parameters-path "$@"
