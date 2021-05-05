bca75f63a9a5bac53f570a2f905d85f360d45f67
George Bochileanu Parfenie <george.constantin.bochileanu@estudiantat.upc.edu>
2021-05-05 22:41:04 +0200

    Changes Spark configuration to make the DatasetsProfiler work for Thunderbird dataset

--- scripts/check_parsed_dataset.py
+++ scripts/check_parsed_dataset.py
@@ -5,7 +5,14 @@ parquet_path = input("Insert path to parquet file (use prefix file:/// or hdfs:/
 spark = SparkSession \
     .builder \
     .appName("Python Spark SQL basic example") \
-    .config("master", "local[*]") \
+    .config("master", "client") \
+    .config("spark.driver.memory", "2526M") \
+    .config("spark.yarn.driver.memoryOverhead", "512M") \
+    .config("spark.executor.memory", "2526M") \
+    .config("spark.yarn.executor.memoryOverhead", "512M") \
+    .config("spark.driver.cores", 1) \
+    .config("spark.executor.cores", 1) \
+    .config("spark.executor.instances", 23) \
     .getOrCreate()
 
 parquetFile = spark.read.parquet(parquet_path)
--- scripts/run-cluster.sh
+++ scripts/run-cluster.sh
@@ -7,9 +7,9 @@ pip3 install --force-reinstall $DATASETS_PROFILER_WHEEL_PATH
 spark-submit \
 --master yarn \
 --deploy-mode client \
---conf spark.driver.memory="1536M" \
+--conf spark.driver.memory="2526M" \
 --conf spark.driver.memoryOverhead="512M" \
---conf spark.executor.memory="1536M" \
+--conf spark.executor.memory="2526M" \
 --conf spark.executor.memoryOverhead="512M" \
 --conf spark.driver.cores="1" \
 --conf spark.executor.cores="1" \
